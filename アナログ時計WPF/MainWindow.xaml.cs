374595cb4c2a02706bef2261923105bbddef3cec
笠松 茂信 <shigenobu314@yahoo.co.jp>
2018-10-12 22:33:31 +0900

    枠なし、ドラッグ移動、コンテキストメニュー閉じる

--- アナログ時計WPF/MainWindow.xaml.cs
+++ アナログ時計WPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,21 @@ namespace アナログ時計WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            //WM_NCLBUTTONDOWN=0xA1, HTCAPTION=2
+            IntPtr hwnd = new System.Windows.Interop.WindowInteropHelper(this).Handle;
+            PostMessage(hwnd, 0xA1, (IntPtr)2, IntPtr.Zero);
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
