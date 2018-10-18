1bc8bc3e8142deda6a8ab97faa020183cca5b1c6
shigobu <shigobu@shigobu.com>
2018-10-18 13:11:25 +0900

    文字大きさ自動

--- アナログ時計WPF/MainWindow.xaml.cs
+++ アナログ時計WPF/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -303,4 +305,19 @@ namespace アナログ時計WPF
             settingWindow.Show();
         }
     }
+
+    class OneParTenConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)value / 10;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return 0;
+        }
+
+    }
+
 }
